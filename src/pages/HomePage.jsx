870fe87bfed19a1ce477eebc9b34ad05a54f4bdc
skakigi <sorakakigi@gmail.com>
2026-04-04 19:27:12 -0400

    grid was oversized + mainpage font changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,8 +24,8 @@ export default function HomePage() {
             textAlign: 'center',
           }}
         >
-          <h1 style={{ margin: 0, fontSize: '3rem' }}>Choose a side</h1>
-          <p style={{ margin: '8px 0 0 0', fontSize: '2rem'
+          <h1 style={{ margin: 0, fontSize: '2rem' }}>Choose a side</h1>
+          <p style={{ margin: '8px 0 0 0', fontSize: '1rem'
            }}>
             Explore engineering or photography
           </p>
